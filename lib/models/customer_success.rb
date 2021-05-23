e4761430798431cea20e9ead301b2c1fa572dfb3
Daniel Landi <dalasa.pifio@gmail.com>
2021-05-23 01:35:35 -0300

    Cria metodo que adiciona e conta clientes sob o atendimento de um cs

--- lib/models/customer_success.rb
+++ lib/models/customer_success.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class CustomerSuccess
-  attr_reader :id, :nivel
+  attr_reader :id, :level, :attended_customers_qty, :attended_customers
 
-
-  def initialize(id, nivel)
+  def initialize(id, level)
     raise ArgumentError, 'ID deve ser numérico e estar entre 0 e 1.000' unless (0..1_000).include?(id)
-    raise ArgumentError, 'O nivel do CS deve ser numérico e estar entre 0 e 10.000' unless (0..10_000).include?(nivel)
+    raise ArgumentError, 'O level do CS deve ser numérico e estar entre 0 e 10.000' unless (0..10_000).include?(level)
 
     @id = id
-    @nivel = nivel
+    @level = level
+    @attended_customers_qty = 0
+    @attended_customers = []
+  end
+
+  def add_attended_customer(customer)
+    attended_customers.push(customer)
+    @attended_customers_qty += 1
   end
 end
--- tests/models/customer_success_test.rb
+++ tests/models/customer_success_test.rb
@@ -5,39 +5,56 @@ require 'minitest/autorun'
 
 # rubocop:disable Metrics/BlockLength
 describe CustomerSuccess do
-  describe 'quando cria uma nova instancia' do
-    subject { CustomerSuccess.new(id, nivel) }
+  describe 'creating a new instance' do
+    subject { CustomerSuccess.new(id, level) }
 
     let(:id) { 10 }
-    let(:nivel) { 100 }
+    let(:level) { 100 }
 
-    describe 'com dados corretos' do
-      it 'retorna uma nova instancia com o id informado' do
+    describe 'using correct data' do
+      it 'returns a new customer success instance using correct id' do
         _(subject.id).must_equal(id)
       end
 
-      it 'retorna uma nova instancia com o nivel informado' do
-        _(subject.nivel).must_equal(nivel)
+      it 'returns a new customer success instance using correct level' do
+        _(subject.level).must_equal(level)
       end
     end
 
-    describe 'com dados incorretos' do
-      describe 'com um id fora do range' do
+    describe 'with invalid data' do
+      describe 'using out of range id' do
         let(:id) { -1 }
 
-        it 'lança um ArgumentError com a mensagem correta' do
+        it 'throws an ArgumentError containing the right message' do
           assert_raises(ArgumentError, 'ID deve ser numérico e estar entre 0 e 1.000') { subject }
         end
       end
 
-      describe 'com um nivel fora do range' do
-        let(:nivel) { '-1' }
+      describe 'using out of range level' do
+        let(:level) { '-1' }
 
-        it 'lança um ArgumentError com a mensagem correta' do
-          assert_raises(ArgumentError, 'O nivel do CS deve ser numérico e estar entre 0 e 10.000') { subject }
+        it 'throws an ArgumentError containing the right message' do
+          assert_raises(ArgumentError, 'O level do CS deve ser numérico e estar entre 0 e 10.000') { subject }
         end
       end
     end
+  end
+
+  describe 'when adds a new attended customer' do
+    let(:cs) { CustomerSuccess.new(1, 1) }
+    let(:customer) { [id: 1, size: 40] }
+
+    subject { cs.add_attended_customer(customer) }
+
+    it 'adds customer to the css custumer list' do
+      subject
+      _(cs.attended_customers).must_include(customer)
+    end
+
+    it 'count +1 on attended customers quantity' do
+      subject
+      _(cs.attended_customers_qty).must_equal(1)
+    end    
   end
 end
 # rubocop:enable Metrics/BlockLength
